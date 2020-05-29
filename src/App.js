fea7dbc0742302c1761155ad5accaa5831f81b5f
Dan Shrout <danshrout@yahoo.com>
2020-05-29 07:40:36 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Website under contruction.
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
