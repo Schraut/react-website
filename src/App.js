a18169df823cc6162066ea81782aeb92de44b876
Dan Shrout <danshrout@yahoo.com>
2020-06-09 19:42:38 -0500

    install react-bootstrap, add card component with link and description of new-threads-clothing react app.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+import  Button  from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
+class App extends React.Component {
+  
+  render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -11,8 +18,21 @@ function App() {
           Website under contruction.
         </h1>
       </header>
+      <h1>Portfolio</h1>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={logo} />
+          <Card.Body>
+            <Card.Title>New Threads Clothing</Card.Title>
+            <Card.Text>
+              A web app built with React. <a href='https://new-threads-clothing.herokuapp.com/'>Click here to see it</a>
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
     </div>
+    
   );
+  }
 }
 
 export default App;
---
+++ src/components/anything/anything.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Anything = () => {
+    <h1>Hey Im learning react</h1>
+}
+
+export default Anything;
